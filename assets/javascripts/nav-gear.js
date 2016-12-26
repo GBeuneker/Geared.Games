94e06128ee4441e05f500251afcdd03156f03963
LAPTOP\Geert Beuneker <g.beuneker@gmail.com>
2016-12-26 14:47:23 +0100

    Navbar now starts at the right place on a page.

--- assets/javascripts/nav-gear.js
+++ assets/javascripts/nav-gear.js
@@ -1,31 +1,76 @@
 var animationLength = 500;
+var activeElement;
+var gearElement;
 
+/*-----------------------------------------------------------------------------
+DOCUMENT EVENTS
+-----------------------------------------------------------------------------*/
 $(function(){
-  $(".nav-element").click(function(e){
-    var positionDiff = $(this).offset().left - $("#nav-gear").offset().left;
+  activeElement = $("#active");
+  gearElement = $("#nav-gear");
 
-    Rotate((positionDiff > 0?1:-1)*180, $("#nav-gear"));
-    var offset = $(this).offset();
-    Translate($(this).offset().left +$(this).outerWidth()/2 - $("#nav-gear").outerWidth()/2, $("#nav-gear"));
+  // Place gear at the right position
+  UpdateGearPosition(GetTargetPos());
+
+
+  // Click on a link
+  $(".nav-element").click(function(e){
+    activeElement = $(this);
+
+    var targetLocation = $(this).offset().left +$(this).outerWidth()/2 - gearElement.outerWidth()/2;
+    MoveGearTo(targetLocation);
+
+    // Delay the page redirect
+    e.preventDefault();
+    DelayRedirect($(this).attr('href'));
   });
 });
 
+$(window).on('resize', function(){
+  UpdateGearPosition();
+});
 
-function Rotate(degree, element)
+/*-----------------------------------------------------------------------------
+FUNCIONS
+-----------------------------------------------------------------------------*/
+function UpdateGearPosition()
+{
+  var targetPos = activeElement.offset().left + activeElement.outerWidth()/2 - gearElement.outerWidth()/2;
+
+  gearElement.css({
+    left: targetPos
+  });
+}
+
+function RotateGear(degree)
 {
   $({deg: 0}).animate({deg: degree}, {
         duration: animationLength,
         step: function(now){
-            element.css({
+            gearElement.css({
                  transform: "rotate(" + now + "deg)"
             });
         }
     });
 }
 
-function Translate(position, element)
+function MoveGearTo(position)
 {
-  element.animate({
+  var posDiff = gearElement.offset().left - position;
+
+  gearElement.animate({
     left: position,
   }, animationLength);
+
+  RotateGear(posDiff);
+}
+
+function DelayRedirect(link)
+{
+  setTimeout(function() {window.location = link}, animationLength);
+}
+
+function GetTargetPos()
+{
+  return activeElement.offset().left + activeElement.outerWidth()/2 - gearElement.outerWidth()/2;
 }
