791d7ce35eddefbfc83e7c4398df579c25c269e3
GEERT-DESKTOP <g.beuneker@gmail.com>
2017-04-30 12:42:41 +0200

    Improved mobile responsive design

--- assets/javascripts/tiles.js
+++ assets/javascripts/tiles.js
@@ -20,19 +20,12 @@ function resizeTiles()
   if(width < 1010)
   {
     $(".tile").each(function(index){
-      $(this).css("width", 400 + "pt");
-      $(this).css("height", 400 + "pt");
       $(this).removeClass("col-md-4");
     });
-    $(".tiles").css("text-align", "center");
-
   }
   else {
     $(".tile").each(function(index){
-      $(this).css("width", 250 + "pt");
-      $(this).css("height", 300 + "pt");
       $(this).addClass("col-md-4");
     });
-    $(".tiles").css("text-align", "");
   }
 }
